5079cac289e563452abc2f5591d562f72f32eeac
stefanleipold <stefan.leipold@yahoo.de>
2015-12-29 10:12:31 +0100

    Create myping.bat

---
+++ myping.bat
@@ -0,0 +1,10 @@
+@echo off
+cls
+color 02
+set myinput=""
+mode con:cols=50 lines=10
+set /p myinput=Hostname: 
+:restart
+ping -t -n 5 %myinput%
+echo Have 5 packets sent - will restart!
+goto restart
